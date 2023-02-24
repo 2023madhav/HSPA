45f6d9c9fd78d97f81a8e89799c2aa4cc0b95dd2
madhav <madhavjoshi13@gmail.com>
2023-02-24 09:26:36 +0530

    test

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import {Iproperty} from 'src/app/Property/Iproperty.interface.ts';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,6 +10,19 @@ export class HousingService {
   constructor(private http:HttpClient) { }
 
   getAllproperties(){
-    return this.http.get('assets/properties.json');
+    return this.http.get('assets/properties.json').pipe(
+      map(data =>{
+            const propertyarry: Array<Iproperty> = [];
+
+            for(const id in data)
+            {
+              if(data.hasOwnProperty(id))
+              {
+                propertyarry.push(data[id] );
+              }
+            }
+            return propertyarry;
+      })
+    );
   }
 }
